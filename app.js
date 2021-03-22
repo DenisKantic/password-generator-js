cc71c2dda162e5b394fc80c17a7f02fda493a1b7
DenisKantic <denis.kantic18@gmail.com>
2021-03-22 13:04:23 +0100

    small changes in html and css file and created const variables with id selectors and variables for eventl...

---
+++ app.js
@@ -0,0 +1,15 @@
+const passwordDisplay = document.getElementById("passwordDisplay");
+const includeLowercase = document.getElementById("includeLowercase");
+const includeUppercase = document.getElementById("includeUppercase");
+const includeSymbols = document.getElementById("includeSymbols");
+const includeNumbers = document.getElementById("includeNumbers");
+const generateButton = document.getElementById("generateBtn");
+
+
+generateButton.addEventListener("click", ()=>{
+
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let numbers = "0123456789";
+    let symbols = "!§$%&/()=";
+});
